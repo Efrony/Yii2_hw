21ad09a437c42a33c1be2414862b920d81dec84e
efrony <efron.vit@gmail.com>
2019-11-24 17:59:59 +0300

    Lesson_2 after

--- models/Activity.php
+++ models/Activity.php
@@ -4,7 +4,15 @@
 namespace app\models;
 
 
-class Activity
-{
+use yii\base\Model;
 
+class Activity extends Model
+{
+    public $title;
+    public $dayStart;
+    public $dayEnd;
+    public $userID;
+    public $description;
+    public $repeat = false;
+    public $blocked;
 }
