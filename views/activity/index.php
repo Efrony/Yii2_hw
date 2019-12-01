41f3b4d333ab9958ec759526311bdfa2e1489469
efrony <efron.vit@gmail.com>
2019-12-01 20:43:26 +0300

    ...work on lesson...

--- views/activity/index.php
+++ views/activity/index.php
@@ -1,33 +1,20 @@
 <?php
 
-use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 
 /**
  * @var $this \yii\web\View
- * @var $model \app\models\Activity
- * @var $form \yii\widgets\ActiveForm
+ * @var array $activities
  */
 ?>
 
+<div class="row">
     <h1>Список событий</h1>
-
-<?php $form = ActiveForm::begin(['action' => ['/activity/create']]); ?>
-
-    <div class="form-group" style="margin-top: 40px; margin-left: 80%">
-        <?= Html::submitButton('Создать', ['class' => 'btn btn-success']) ?>
+    <div class="form-group pull-right">
+        <?= Html::a('Создать', ['activity/create'], ['class' => 'btn btn-success pull-right']) ?>
     </div>
-
-<?php ActiveForm::end(); ?>
-
-<?php $form = ActiveForm::begin(['action' => ['/activity/view']]); ?>
-<div class="form-group" style="margin-top: 60px;">
-    <?= Html::submitButton('Событие_1', ['class' => 'btn btn-success']) ?>
 </div>
-<?php ActiveForm::end(); ?>
 
-<?php $form = ActiveForm::begin(['action' => ['/activity/view']]); ?>
-<div class="form-group" style="margin-top: 60px;">
-    <?= Html::submitButton('Событие_2', ['class' => 'btn btn-success']) ?>
-</div>
-<?php ActiveForm::end(); ?>
+<?php foreach ($activities as $item): ?>
+    <?= var_dump($item)?>
+<?php endforeach;?>
