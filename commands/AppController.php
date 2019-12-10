611453da72753ad41464958647e2a4205d4478d5
Efrony <efron.vit@gmail.com>
2019-12-10 13:53:05 +0300

    ...

--- commands/AppController.php
+++ commands/AppController.php
@@ -4,24 +4,62 @@
 namespace app\commands;
 
 
+use app\models\Activity;
 use app\models\User;
 use yii\console\Controller;
 
 class AppController extends Controller
 {
+    /**
+     * Создание начальных пользователей
+     * php yii app/users
+     */
     public function actionUsers()
     {
-        $admin = new User([
-            'username' => 'admin',
-            //'password_hash' => '',
-            //'auth_key' => '',
-            'access_token' => 'test',
-            'created_at' => time(),
-            'updated_at' => time(),
-        ]);
+        $users = [
+            'admin',
+            'manager',
+            'user',
+        ];
 
-        $admin->generateAuthKey();
-        $admin->password = '123'; // setPassword('123')
-        $admin->save();
+        foreach ($users as $login) {
+            $user = new  User([
+                'username' => $login,
+                'access_token' => "{$login}-token",
+                'created_at' => time(),
+                'updated_at' => time(),
+            ]);
+
+            $user->generateAuthKey();
+            $user->password = '123'; // setPassword('123')
+            $user->save();
+        }
+    }
+
+    public function actionActivities()
+    {
+        $titles = [
+            'Первое событие',
+            'Второе событие',
+            'Третье событие',
+        ];
+
+        $day = 1;
+        $today = time();
+
+        foreach ($titles as $title) {
+            $activityDate = date('Y-m-d', strtotime("+ {$day} days", $today));
+            $activity = new Activity([
+                'title' => $title,
+                'description' => chunk_split(\Yii::$app->security->generateRandomString(64), random_int(10, 20), ' '),'title' => $title,
+                'user_id' => random_int(1,3),
+                'day_start' => $activityDate,
+                'day_end' => $activityDate,
+                'blocked' => random_int(0,1),
+                'repeat' => false,
+            ]);
+            $activity->save();
+            $day++;
+        }
     }
 }
--- commands/RbacController.php
+++ commands/RbacController.php
@@ -8,15 +8,45 @@ use yii\console\Controller;
 
 class RbacController extends Controller
 {
+    /**
+     * php yii rbac/init
+     */
+
     public function actionInit()
     {
+        // аналогично выполнению в терминале 'php yii migrate --migrationPath=@yii/rbac/migrations
+        \Yii::$app->runAction('migrate', ['migratePath' => '@yii/rbac/migrations']);
+
+        //компонент управления RBAC
         $auth = \Yii::$app->authManager;
 
-        // добавляем разрешение "createPost"
-        $adminRole = $auth->createRole('admin');
-        $adminRole->description = 'super admin';
-        $auth->add($adminRole);
+        /**
+         * Создание ролей пользователей
+         */
+        // обычный пользователь
+        $roleUsers = $auth->createRole('user');
+        $roleUsers->description = 'Обычный пользователь';
+        $auth->add($roleUsers);
 
-        $auth->assign($adminRole, 1);
+        // менеджер
+        $roleManager = $auth->createRole('manager');
+        $roleManager->description = 'Менеджер';
+        $auth->add($roleManager);
+        $auth->addChild($roleManager, $roleUsers); // Менеджер наследует права Пользователя
+
+
+        // Администратор
+        $roleAdmin = $auth->createRole('admin');
+        $roleAdmin->description = 'Администратор';
+        $auth->add($roleAdmin);
+        $auth->addChild($roleAdmin, $roleManager); // Администратор наследует права Менеджера
+
+        /**
+         * Установка ролей для пользователей
+         */
+
+        $auth->assign($roleAdmin, 1);
+        $auth->assign($roleManager, 2);
+        $auth->assign($roleUsers, 3);
     }
 }
